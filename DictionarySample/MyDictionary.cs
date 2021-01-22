f07a37fcc9e0ebfd5b956641f78302c9a93e5757
has ozg <hasan_ozgen@windowslive.com>
2021-01-23 00:47:19 +0300

    Add project files.

---
+++ DictionarySample/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionarySample
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] arrayKey;
+        T2[] arrayValue;
+        public MyDictionary()
+        {
+            arrayKey = new T1[0] ;
+            arrayValue = new T2[0] ;
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempKey = arrayKey;
+            T2[] tempValue = arrayValue;
+
+            arrayKey = new T1[arrayKey.Length + 1];
+            arrayValue = new T2[arrayValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                arrayKey[i] = tempKey[i];
+                arrayValue[i] = tempValue[i];
+            }
+
+            arrayKey[arrayKey.Length - 1] = key;
+            arrayValue[arrayValue.Length - 1] = value;
+            
+        }
+
+        public T1[] Keys
+        {
+            get { return arrayKey; }
+        }
+        public T2[] Values
+        {
+            get { return arrayValue; }
+        }
+    }
+}
---
+++ DictionarySample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionarySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> Ogrenci1 = new Dictionary<string, string>();
+
+            Ogrenci1.Add("Hoca", "Engin Demirog");
+            Ogrenci1.Add("Öğrenci", "Emre Turan");
+
+            Console.WriteLine(Ogrenci1["Hoca"]);
+
+            Console.WriteLine("-------Benim Yapım-------");
+
+            MyDictionary<string, string> Ogrenci2 = new MyDictionary<string, string>();
+
+            Ogrenci2.Add("Hoca", "Engin Demirog");
+            Ogrenci2.Add("Öğrenci", "Emre Turan");
+
+            Console.WriteLine(Ogrenci2.Keys[0] +":"+ Ogrenci2.Values[0]);
+            Console.WriteLine(Ogrenci2.Keys[1] +":"+ Ogrenci2.Values[1]);
+
+
+
+
+        }
+    }
+}
